d2b34063a2824e380ee895468625bfcb044e9cb5
Harshit Pandey <harshpandeyalfa2@gmail.com>
2023-03-16 07:50:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) displayMessage('⛔️ No number!');
+  else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+      guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+    else {
+      displayMessage('👎👎 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+      alert("Click on Again To Restart");
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+// // Coding Challenge #1
+
+// /*
+// Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the score and secretNumber variables
+// 3. Restore the initial conditions of the message, number, score and guess input field
+// 4. Also restore the original background color (#222) and number width (15rem)
+
+// GOOD LUCK 😀
+// */
